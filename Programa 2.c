78af54b81a76e47342c95ff028425015f89aaee6
Fredycuellar <80786325+Fredycuellar@users.noreply.github.com>
2021-05-19 19:25:31 -0500

    Update Programa 2.c

--- Programa 2.c
+++ Programa 2.c
@@ -1,14 +1,14 @@
-1.	#include <wiringPi.h>
-2.	int main (void)
-3.	{
-4.	        wiringPiSetup();
-5.	        pinMode(29, OUTPUT);
-6.	        int i= 0;
-7.	        for (i = 0;i<100;i++){
-8.	                digitalWrite(29, HIGH);
-9.	                delay(5000);
-10.	                digitalWrite(29, LOW);
-11.	                delay(5000);
-12.	        }
-13.	        return 0 ;
-14.	}
+	#include <wiringPi.h>
+	int main (void)
+	{
+	        wiringPiSetup();
+	        pinMode(29, OUTPUT);
+	        int i= 0;
+	        for (i = 0;i<100;i++){
+	                digitalWrite(29, HIGH);
+	                delay(5000);
+	                digitalWrite(29, LOW);
+	                delay(5000);
+	        }
+	        return 0 ;
+	}
