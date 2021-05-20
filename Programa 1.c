46bb3b18a1958ade8ee0513449268d17c50c7845
Fredycuellar <80786325+Fredycuellar@users.noreply.github.com>
2021-05-19 19:24:57 -0500

    Update Programa 1.c

--- Programa 1.c
+++ Programa 1.c
@@ -1,14 +1,14 @@
-1.	#include <wiringPi.h>
-2.	int main (void)
-3.	{
-4.	        wiringPiSetup();
-5.	        pinMode(0, OUTPUT);
-6.	        int i= 0;
-7.	        for (i = 0;i<100;i++){
-8.	                digitalWrite(0, HIGH);
-9.	                delay(5000);
-10.	                digitalWrite(0, LOW);
-11.	                delay(5000);
-12.	        }
-13.	        return 0 ;
-14.	}
+#include <wiringPi.h>
+	int main (void)
+	{
+	        wiringPiSetup();
+	        pinMode(0, OUTPUT);
+	        int i= 0;
+	        for (i = 0;i<100;i++){
+	                digitalWrite(0, HIGH);
+	                delay(5000);
+	                digitalWrite(0, LOW);
+	                delay(5000);
+	        }
+	        return 0 ;
+	}
